1e170c965985d069ffb713554ce0a8639e6adb35
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-19 03:23:36 -0700

    Initial repository setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Shift',
+      version='0.0.1',
+      description='A generic template library for Python',
+      author='Andrew Dunham',
+      url='http://github.com/andrew-d/Shift',
+      license='Apache',
+      platforms='any',
+      zip_safe=False,
+      packages=['shift'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+     )
+
