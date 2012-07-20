682f6b9aa0de5b4fee933f309dc4cf3118ed3381
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-20 02:05:15 -0700

    Initial implementation of template registry

--- shift/base.py
+++ shift/base.py
@@ -1,11 +1,40 @@
 from __future__ import with_statement
 
 
+class Shift(object):
+    """
+    This class implements a template registry, where individual template
+    classes can register themselves to handle template types.
+    """
+    def __init__(self):
+        raise RuntimeError("Shift is a static class - do not create!")
+
+    @staticmethod
+    def new(template_path):
+        pass
+
+    @staticmethod
+    def register_class(klass):
+        print "Registering {0!r}...".format(klass)
+
+
+class BaseTemplateMetaclass(type):
+    def __init__(klass, name, bases, attrs):
+        # If this class is not a base class, we add it to the registry.
+        if not klass.__name__ == 'BaseTemplate':
+            Shift.register_class(klass)
+
+        # Call the base __new__ to finish creating the class.
+        return type.__init__(klass, name, bases, attrs)
+
+
 class BaseTemplate(object):
     """
     This is the base template class.  It specifies the interface that template
     renderers must conform to.
     """
+    __metaclass__ = BaseTemplateMetaclass
+
     def __init__(self, file_path=None):
         self.file_path = file_path
 
@@ -31,16 +60,4 @@ class BaseTemplate(object):
         """
         return self.on_render(context, string, *args, **kwargs)
 
-
-class Shift(object):
-    """
-    This class implements a template registry, where individual template
-    classes can register themselves to handle template types.
-    """
-    def __init__(self):
-        raise RuntimeError("Shift is a static class - do not create!")
-
-    @staticmethod
-    def new(template_path):
-        pass
 
