cd5c03b18023bf83cc96c0bd353a5b156d619618
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-21 19:40:29 -0700

    Added misaka renderer for markdown

--- shift/engines/__init__.py
+++ shift/engines/__init__.py
@@ -8,12 +8,15 @@ from .coffeescript import CoffeeScriptTemplate
 from .lessc import LessCSSTemplate
 from .mako import MakoTemplate
 from .markdown import MarkdownTemplate
+from .misaka import MarkdownMisakaTemplate
 
 
 # Import the registry class.
 from ..base import Shift
 
 # Register all the classes.
+# NOTE: Order does matter - the registration order defines the order
+# in which template engines are tried (first to last).
 Shift.register_class(BareTemplate, 'bare')
 Shift.register_class(JinjaTemplate, 'jinja2')
 Shift.register_class(MustacheTemplate, 'mustache')
@@ -23,6 +26,8 @@ Shift.register_class(CoffeeScriptTemplate, 'coffee')
 Shift.register_class(CoffeeScriptTemplate, 'coffeescript')
 Shift.register_class(LessCSSTemplate, 'less')
 Shift.register_class(MakoTemplate, 'mako')
+Shift.register_class(MarkdownMisakaTemplate, 'markdown')
+Shift.register_class(MarkdownMisakaTemplate, 'md')
 Shift.register_class(MarkdownTemplate, 'markdown')
 Shift.register_class(MarkdownTemplate, 'md')
 
---
+++ shift/engines/markdown_misaka.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from ..base import Shift, BaseTemplate
+
+class MarkdownMisakaTemplate(BaseTemplate):
+    def on_render(self, template, context):
+        return self.renderer(template)
+
+    @classmethod
+    def on_initialize(klass):
+        try:
+            import misaka
+        except ImportError:
+            return False
+
+        klass.renderer = misaka.html
+        return True
+
