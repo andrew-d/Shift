995f15d603e873bb9b7aa97bbf1f66787d1e92e1
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-21 23:16:59 -0700

    Patch sys.stderr when import pyScss to avoid the error message when it can't find the C extension

--- shift/engines/scss.py
+++ shift/engines/scss.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from ..base import Shift, BaseTemplate
+import sys
+import StringIO
 
 class ScssTemplate(BaseTemplate):
     def on_render(self, template, context):
@@ -7,10 +9,18 @@ class ScssTemplate(BaseTemplate):
 
     @classmethod
     def on_initialize(klass):
+        # We patch sys.stderr here, since pyScss insists on printing to stderr
+        # if it can't find the C extensions.
+        old_err = sys.stderr
+
         try:
-            import scss
-        except ImportError:
-            return False
+            sys.stderr = StringIO.StringIO()
+            try:
+                import scss
+            except ImportError:
+                return False
+        finally:
+            sys.stderr = old_err
 
         klass.renderer = scss.Scss()
         return True
