5ef77d9a1982f58fded4e94b0da673a12c44d123
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-22 23:08:24 -0700

    Initial refactoring of Jinja2 loader

--- shift/engines/jinja2.py
+++ shift/engines/jinja2.py
@@ -2,9 +2,21 @@ from __future__ import absolute_import
 from ..base import Shift, BaseTemplate
 
 class JinjaTemplate(BaseTemplate):
-    def on_render(self, template, context):
-        renderer = self.Template(template)
-        return renderer.render(context)
+    def load_string(self, template):
+        self.renderer = self.Template(template)
+
+    def load_file(self, file_path, root_dir=None, **kwargs):
+        if self.root_dir is not None:
+            encoding = kwargs.pop('encoding', 'utf-8')
+            self.loader = self.FileSystemLoader(self.root_dir, encoding=encoding)
+            self.env = self.Environment(loader=self.loader, **kwargs)
+        else:
+            self.env = self.Environment()
+
+        self.renderer = self.env.get_template(file_path)
+
+    def render(self, context=None):
+        return self.renderer.render(context)
 
     @classmethod
     def on_initialize(klass):
@@ -13,6 +25,8 @@ class JinjaTemplate(BaseTemplate):
         except ImportError:
             return False
 
+        klass.Loader = jinja2.FileSystemLoader
+        klass.Environment = jinja2.Environment
         klass.Template = jinja2.Template
         return True
 
