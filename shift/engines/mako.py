28863653a5a97a790044ffda739730cf40e49022
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-23 00:27:50 -0700

    Update Mako template engine

--- shift/engines/mako.py
+++ shift/engines/mako.py
@@ -2,17 +2,27 @@ from __future__ import absolute_import
 from ..base import Shift, BaseTemplate
 
 class MakoTemplate(BaseTemplate):
-    def on_render(self, template, context):
-        renderer = self.Template(template)
-        return renderer.render(**context)
+    def load_string(self, template):
+        self.renderer = self.Template(template)
+
+    def load_file(self, file_path, root_dir=None):
+        path = root_dir or '.'
+        self.loader = self.TemplateLookup(directories=[path])
+        self.renderer = self.loader.get_template(file_path)
+
+    def render(self, context=None):
+        ctx = context or {}
+        return self.renderer.render(**ctx)
 
     @classmethod
     def on_initialize(klass):
         try:
             import mako.template
+            import mako.lookup
         except ImportError:
             return False
 
+        klass.TemplateLookup = mako.lookup.TemplateLookup
         klass.Template = mako.template.Template
         return True
 
