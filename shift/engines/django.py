8831cc8f922b858045e734b0561224324d0d4e1c
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-22 23:48:59 -0700

    Updated Django template engine

--- shift/engines/django.py
+++ shift/engines/django.py
@@ -2,18 +2,28 @@ from __future__ import absolute_import
 from ..base import Shift, BaseTemplate
 
 class DjangoTemplate(BaseTemplate):
-    def on_render(self, template, context):
-        renderer = self.Template(template)
+    def load_string(self, template):
+        self.renderer = self.Template(template)
+
+    def load_file(self, file_path, root_dir=None):
+        root_dir = root_dir or '.'
+        self.settings.configure(TEMPLATE_DIRS=(root_dir,))
+        self.renderer = self.get_template(file_path)
+
+    def render(self, context=None):
         context = self.Context(context)
-        return renderer.render(context)
+        return self.renderer.render(context)
 
     @classmethod
     def on_initialize(klass):
         try:
             import django.template
+            from django.conf import settings
         except ImportError:
             return False
 
+        klass.settings = settings
+        klass.get_template = django.template.loader.get_template
         klass.Template = django.template.Template
         klass.Context = django.template.Context
         return True
