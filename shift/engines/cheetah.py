b64e570dbf2139af9d64af554dcb183ea3f788bb
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-07-26 00:29:11 -0700

    Catch the warning from Cheetah about a missing C extension

--- shift/engines/cheetah.py
+++ shift/engines/cheetah.py
@@ -1,13 +1,18 @@
 from __future__ import absolute_import
 from ..base import Shift, BaseTemplate
+import warnings
 
 class CheetahTemplate(BaseTemplate):
     def load_string(self, template):
         self.string = template
 
     def render(self, context=None):
-        renderer = self.Template(self.string, searchList=[context])
-        return str(renderer)
+        # We catch warnings here so we don't get the irritating "you don't
+        # have the C extension installed" warning.
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            renderer = self.Template(self.string, searchList=[context])
+            return str(renderer)
 
     @classmethod
     def on_initialize(klass):
